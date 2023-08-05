be02cd24e34e4742c2d4ed8babd6970ee3e05010
Jyoti Pandey <rajoriyadev11@gmail.com>
2023-08-05 16:08:56 +0530

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,13 @@ import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
-import "./home.css"
+import "./home.css";
+import { useNavigate } from "react-router-dom";
 
 
 
 export default function Home() {
 
-  
 
   return (
     <>
